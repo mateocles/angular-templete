932b3482135b8861ec6fac9f5acb8c97dcb8b629
mateocles <mat.bermudez@udla.edu.co>
2023-02-18 18:54:28 -0500

    add: component angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
