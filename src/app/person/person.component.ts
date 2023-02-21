727989c4e0d6c75832dba3d9033ea2252dba3e2e
mateocles <mat.bermudez@udla.edu.co>
2023-02-21 16:00:24 -0500

    add interpolacion component

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.css'],
 })
 export class PersonComponent {
-
+  name: string = 'Mateo';
+  lastname: string = 'Ceballos';
+  private years: number = 26;
+  getYears(): number {
+    return this.years;
+  }
 }
--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
-  styleUrls: ['./persons.component.css']
+  styleUrls: ['./persons.component.css'],
 })
 export class PersonsComponent {
-
+  disabled = false;
+  message = 'No ha agregado ninguna persona';
+  title = '';
+  addPerson() {
+    this.message = 'Se ha agrego una persona';
+    console.log('add');
+  }
+  changeTitle(e: Event) {
+    this.title = 	(<HTMLInputElement> e.target).value;
+  }
 }
